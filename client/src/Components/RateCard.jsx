6870193382c184340b50cf045547ca0d2a91d9b3
MIR TAUHIDUL ISLAM <dev.tauhidul@gmail.com>
2021-09-25 08:32:42 +0600

    restaurant info page ready

--- client/src/Components/RateCard.jsx
+++ client/src/Components/RateCard.jsx
@@ -1,42 +1,37 @@
+import moment from 'moment';
 import React from 'react';
+import Datetime from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
+import ReactStars from 'react-rating-stars-component';
 import '../Styles/RateCard.css';
 
 const RateCard = () => {
+  let inputProps = {
+    placeholder: '  Select visit date',
+  };
+
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
   return (
     <div>
-      <form id='feedback' action>
-        <div className='pinfo'>Write your feedback.</div>
-        <div className='form-group'>
-          <div className='col-md-4 inputGroupContainer'>
-            <div className='input-group'>
-              <span className='input-group-addon'>
-                <i className='fa fa-heart' />
-              </span>
-              <select className='form-control' id='rate'>
-                <option value='1star'>1</option>
-                <option value='2stars'>2</option>
-                <option value='3stars'>3</option>
-                <option value='4stars'>4</option>
-                <option value='5stars'>5</option>
-              </select>
-            </div>
-          </div>
-        </div>
-        <div className='form-group'>
-          <div className='col-md-4 inputGroupContainer'>
-            <div className='input-group'>
-              <span className='input-group-addon'>
-                <i className='fa fa-envelope' />
-              </span>
-              <input
-                name='email'
-                type='email'
-                className='form-control'
-                placeholder='john.doe@yahoo.com'
-              />
-            </div>
-          </div>
-        </div>
+      <form style={{ minWidth: '400px' }} id='feedback' action>
+        <div className='pinfo'>Write your review.</div>
+        <ReactStars
+          count={5}
+          onChange={ratingChanged}
+          size={24}
+          activeColor='#ffd700'
+        />
+        <br />
+        <Datetime
+          inputProps={inputProps}
+          onChange={(event) =>
+            console.log(moment(event._d).format('YYYY-MM-DD'))
+          }
+          timeFormat={false}
+        />
+        <br />
         <div className='pinfo'></div>
         <div className='form-group'>
           <div className='col-md-4 inputGroupContainer'>
